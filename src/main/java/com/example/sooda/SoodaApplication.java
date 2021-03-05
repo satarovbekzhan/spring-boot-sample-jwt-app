53d518a0a157b6abe3b9f34b6c1cf3eccb27b734
Bekzhan Satarov <satarovbekzhan@gmail.com>
2021-03-05 18:01:07 +0100

    Projects initialization

---
+++ src/main/java/com/example/sooda/SoodaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sooda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoodaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sooda/SoodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sooda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
